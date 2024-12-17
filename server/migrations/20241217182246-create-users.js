5b8ab9dec9a9dbe77565228b8d125875ba901546
Ilya Lobanovskiy <90864286+morganizwd@users.noreply.github.com>
2024-12-17 21:50:38 +0300

    feat: user controller + router + migration

---
+++ server/migrations/20241217182246-create-users.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.ENUM('client', 'operator'),
+        defaultValue: 'client',
+        allowNull: false,
+      },
+      createdAt: { // Автоматически добавляются поля createdAt и updatedAt, если timestamps: true
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const authenticateToken = require('../middlewares/authenticateToken'); 
+const authenticateToken = require('../middleware/authenticateToken'); 
 
 router.post('/register', userController.register);
 
